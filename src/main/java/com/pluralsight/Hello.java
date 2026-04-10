e07af65fba208a29deee4862cd697447a8203401
keziazahiljohn <keziazahiljohn@gmail.com>
2026-04-10 09:57:56 -0400

    Finished Hello.java

--- src/main/java/com/pluralsight/Hello.java
+++ src/main/java/com/pluralsight/Hello.java
@@ -1,4 +1,20 @@
 package com.pluralsight;
 
 public class Hello {
+    public static void main(String[] args) {
+
+    }
+
+    public static void helloWorld(String[] args) {
+        System.out.println("Hello World!");
+    }
+
+    public static void sayGoodbye(String[] args) {
+        System.out.println("Goodbye!");
+    }
+
+    public static void sayGoodMorning(String[] args) {
+        System.out.println("Good Morning!");
+    }
+
 }
