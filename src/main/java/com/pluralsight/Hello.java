c44ee312d0beb33a4ed26bb0b2b7a59457a64fc5
keziazahiljohn <keziazahiljohn@gmail.com>
2026-04-10 10:00:56 -0400

    Fixed Hello.java

--- src/main/java/com/pluralsight/Hello.java
+++ src/main/java/com/pluralsight/Hello.java
@@ -2,18 +2,20 @@ package com.pluralsight;
 
 public class Hello {
     public static void main(String[] args) {
-
+        sayHello();
+        sayGoodbye();
+        sayGoodMorning();
     }
 
-    public static void helloWorld(String[] args) {
+    public static void sayHello() {
         System.out.println("Hello World!");
     }
 
-    public static void sayGoodbye(String[] args) {
+    public static void sayGoodbye() {
         System.out.println("Goodbye!");
     }
 
-    public static void sayGoodMorning(String[] args) {
+    public static void sayGoodMorning() {
         System.out.println("Good Morning!");
     }
 
